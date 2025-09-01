1a7bb826d69fad1defd40d61473be0c750a7bb64
John <tukhtamirzaevnazirjon96@gmail.com>
2025-09-01 21:52:30 +0900

    feat: configure env varaiables

--- apps/avto-baraka-batch/src/avto-baraka-batch.module.ts
+++ apps/avto-baraka-batch/src/avto-baraka-batch.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AvtobarakaBatchController } from './avto-baraka-batch.controller';
 import { AvtobarakaBatchService } from './avto-baraka-batch.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()],
   controllers: [AvtobarakaBatchController],
   providers: [AvtobarakaBatchService],
 })
--- apps/avto-baraka-batch/src/main.ts
+++ apps/avto-baraka-batch/src/main.ts
@@ -3,6 +3,6 @@ import { AvtobarakaBatchModule } from './avto-baraka-batch.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AvtobarakaBatchModule);
-  await app.listen(process.env.port ?? 3001);
+  await app.listen(process.env.PORT_BATCH ?? 3000);
 }
 bootstrap();
