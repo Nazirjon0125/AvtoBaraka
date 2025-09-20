3c58b4573e758d61efdcbb121d176bb467d956d6
John <tukhtamirzaevnazirjon96@gmail.com>
2025-09-20 23:29:32 +0900

    feat: develop admin related boardArtcle GraphQl api

--- apps/avto-baraka-api/src/components/board-article/board-article.service.ts
+++ apps/avto-baraka-api/src/components/board-article/board-article.service.ts
@@ -222,7 +222,11 @@ export class BoardArticleService {
 	public async boardArticleStatsEditor(input: StatisticModifier): Promise<BoardArticle> {
 		const { _id, targetKey, modifier } = input;
 		return await this.boardArticleModel
-			.findByIdAndUpdate(_id, { $inc: { [targetKey]: modifier } }, { new: true })
+			.findByIdAndUpdate(
+				_id,
+				{ $inc: { [targetKey]: modifier } }, //
+				{ new: true },
+			)
 			.exec();
 	}
 }
