93d6ead5ae8de0f5c84df41ccc1e5588496442d4
John <tukhtamirzaevnazirjon96@gmail.com>
2025-09-27 21:34:37 +0900

    feat: create batchRollback, batchTopProperties and batchTopAgents jobSchedulers logic

--- apps/avto-baraka-batch/src/batch.service.ts
+++ apps/avto-baraka-batch/src/batch.service.ts
@@ -2,7 +2,19 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class BatchService {
-	getHello(): string {
+	public getHello(): string {
 		return 'Welcome to AvtoBaraka BATCH Server!';
+	}
+
+	public batchRollback(): string {
+		return 'batchRollback!';
+	}
+
+	public batchTopProperties(): string {
+		return 'batchTopProperties!';
+	}
+
+	public batchTopAgents(): string {
+		return 'batchTopAgents!';
 	}
 }
