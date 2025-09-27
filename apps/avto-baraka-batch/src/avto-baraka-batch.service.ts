50b8a2683af1bb8438113f9b7b30e93c6b21533d
John <tukhtamirzaevnazirjon96@gmail.com>
2025-09-27 21:10:04 +0900

    fix: modify batch module and connect to MongoDB

--- apps/avto-baraka-batch/src/avto-baraka-batch.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class AvtobarakaBatchService {
-  getHello(): string {
-    return 'Welcome to AvtoBaraka BATCH Server!';
-  }
-}
--- apps/avto-baraka-batch/src/main.ts
+++ apps/avto-baraka-batch/src/main.ts
@@ -1,8 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-import { AvtobarakaBatchModule } from './avto-baraka-batch.module';
+import { BatchModule } from './avbatch.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AvtobarakaBatchModule);
-  await app.listen(process.env.PORT_BATCH ?? 3000);
+	const app = await NestFactory.create(BatchModule);
+	await app.listen(process.env.PORT_BATCH ?? 3000);
 }
 bootstrap();
